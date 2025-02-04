ac0d02821293bf7cd04dd119529fe57e21fcdf65
llsanketll <slamsal2000@gmail.com>
2025-02-04 10:46:41 +0545

    add  more home url

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./Home";
 import Game from "./Game";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
   { path: "/KodeWords", element: <Home /> },
   { path: "/home", element: <Home /> },
   { path: "/game/:role/:roomid", element: <Game /> },
